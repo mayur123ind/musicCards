14c9ef0a78d5f8b067fd7224b3976cae55f84d2c
mayur <mayur.indrekar07@gmail.com>
2024-05-04 00:03:14 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Navbar from './Components/Navbar'
+import Card from './Components/Card'
 
 const App = () => {
+
+  const data = [
+    {
+      songName: "Tum hi ho",
+      artistName: "Arjit Singh",
+      image:
+        "https://images.unsplash.com/photo-1569128782402-d1ec3d0c1b1b?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      favourites: false,
+    },
+    {
+      songName: "Dil Se Re",
+      artistName: "A.R. Rahman",
+      image:
+        "https://images.unsplash.com/photo-1569128782402-d1ec3d0c1b1b?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      favourites: false,
+    },
+
+    {
+      songName: "Mera Dil",
+      artistName: "Alka Yagnik",
+      image:
+        "https://images.unsplash.com/photo-1569128782402-d1ec3d0c1b1b?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      favourites: false,
+    },
+
+    {
+      songName: "Dil",
+      artistName: "Kumar Sanu",
+      image:
+        "https://images.unsplash.com/photo-1569128782402-d1ec3d0c1b1b?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      favourites: false,
+    },
+    {
+      songName: "Shiv Tandav",
+      artistName: "Shankar Mahadevan",
+      image:
+        "https://images.unsplash.com/photo-1569128782402-d1ec3d0c1b1b?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      favourites: false,
+    },
+
+    {
+      songName: "Haan Tu Hain",
+      artistName: "K.K",
+      image:
+        "https://images.unsplash.com/photo-1569128782402-d1ec3d0c1b1b?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      favourites: false,
+    },
+  ];
+
+  const [songData, setSongData] = useState(data)
+
+  const handleFavButton = (index)=>{
+    // alert(index);
+    setSongData((prev)=>{
+      return prev.map((item,itemIndex)=>{
+        if(itemIndex===index){
+          return {...item,favourites: !item.favourites}
+        }
+        return item;
+      })
+    })
+  }
+
   return (
-    <div>App</div>
+    <>
+    <div className="w-full h-screen bg-zinc-300">
+      <Navbar data={songData}/>
+     <div className="px-20 flex flex-wrap mt-10 gap-10">
+     {songData.map((obj,index)=>(
+      <Card key={index} data={obj} handleFavButton={handleFavButton} index={index} />
+      ))}
+     </div>
+    </div>
+    </>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({data}) => {
   return (
     <div className="w-full px-20 py-3 flex justify-between items-center ">
         <h1 className="text-blue-600 font-extrabold">SongsPk.com</h1>
         <div className="flex gap-2 p-2 px-4 bg-orange-600 text-white rounded-md text-sm">
             <h3 >Favouites</h3>
-            <h4>2</h4>
+            <h4>{data.filter(item => item.favourites).length}</h4>
         </div>
     </div>
   )
